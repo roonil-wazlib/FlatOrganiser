02ada793c3d0e538d3d28d9dbf2559631bcc48a0
roonil-wazlib <elfhogan@gmail.com>
2020-01-13 18:01:24 +1300

    Added some basic layouts to use for testing

--- app/src/main/java/com/emmahogan/flatorganiser/MainActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.emmahogan.flatorganiser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val billsButton: Button = findViewById(R.id.bills_button)
+        billsButton.setOnClickListener { onClick() }
+    }
+
+    fun onClick(){
+        val intent = Intent(this, BillsActivity::class.java)
+        startActivity(intent)
     }
 }
