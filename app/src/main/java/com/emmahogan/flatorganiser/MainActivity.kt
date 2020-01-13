1d2ffe5e11b9da1c0d1458a07671c1fb3c5225c7
roonil-wazlib <elfhogan@gmail.com>
2020-01-13 22:46:20 +1300

    User authentication for creating new account now working

--- app/src/main/java/com/emmahogan/flatorganiser/MainActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/MainActivity.kt
@@ -3,7 +3,17 @@ package com.emmahogan.flatorganiser
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.NonNull
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseUser
+
+
+/*once sign in is working, follow instructions
+in tools - firebase - authentication 3 to check if user is logged in*/
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +21,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val billsButton: Button = findViewById(R.id.signup_button)
-        billsButton.setOnClickListener { signUp() }
+        val signUpButton: Button = findViewById(R.id.signup_button)
+        signUpButton.setOnClickListener { signUp() }
     }
 
     fun signUp(){
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
     }
+
 }
--- app/src/main/java/com/emmahogan/flatorganiser/SignUpActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/SignUpActivity.kt
@@ -2,20 +2,70 @@ package com.emmahogan.flatorganiser
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val billsButton: Button = findViewById(R.id.submit_signup)
-        billsButton.setOnClickListener { signUp() }
+        //Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        val submitButton: Button = findViewById(R.id.submit_signup)
+        submitButton.setOnClickListener { createAccount() }
+
+        val emailET: EditText = findViewById(R.id.email)
+        val nameET: EditText = findViewById(R.id.name)
+        val passwordET: EditText = findViewById(R.id.password)
+        val passwordCheckET: EditText = findViewById(R.id.password_check)
     }
 
-    fun signUp(){
-        //do firebase stuff
+    fun createAccount(){
+        //make a new user
+        val emailET: EditText = findViewById(R.id.email)
+        val nameET: EditText = findViewById(R.id.name)
+        val passwordET: EditText = findViewById(R.id.password)
+        val passwordCheckET: EditText = findViewById(R.id.password_check)
+
+        val email = emailET.getText().toString()
+        val password = passwordET.getText().toString()
+        val passwordCheck = passwordCheckET.getText().toString()
+
+        if (checkPasswords(password, passwordCheck)) {
+            mAuth!!.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task: Task<AuthResult> ->
+                    if (task.isSuccessful) {
+                        //Registration OK
+                        val firebaseUser = mAuth!!.currentUser //throw exception if null
+                        Toast.makeText(this@SignUpActivity, "Appears to be working!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this@SignUpActivity, "Some kind of registration error?", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        else {
+            //passwords don't match
+            Toast.makeText(this@SignUpActivity, "Please check passwords match", Toast.LENGTH_SHORT).show()
+        }
     }
 
+    fun checkPasswords(password: String, passwordCheck: String) : Boolean {
+        //check correct password entered
+        return password == passwordCheck
+
+    }
 }
