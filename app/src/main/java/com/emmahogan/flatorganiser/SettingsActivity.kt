0b3c1d806762b342b113cd2d6bf7c867c9de235a
roonil-wazlib <elfhogan@gmail.com>
2020-01-20 14:40:40 +1300

    Delete account functionality complete

--- app/src/main/java/com/emmahogan/flatorganiser/SettingsActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/SettingsActivity.kt
@@ -1,12 +1,25 @@
 package com.emmahogan.flatorganiser
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SettingsActivity : AppCompatActivity() {
+
+    private var mAuth = FirebaseAuth.getInstance()
+
+    //Initialise Firebase db
+    private var mDatabase = FirebaseDatabase.getInstance()
+
+    //Get reference to users child
+    private var mDatabaseReference = mDatabase!!.reference!!.child("users")
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -25,13 +38,15 @@ class SettingsActivity : AppCompatActivity() {
         //bring up are you sure
 
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Delete Account?")
+        builder.setTitle("Confirm delete account?")
         builder.setMessage("This action is not reversable")
 
 
         builder.setPositiveButton("Continue"){dialog, which ->
-            deleteAccount()
+            deleteAuthAccount()
+            deleteFromDatabase()
             Toast.makeText(this, "Deleting account...", Toast.LENGTH_SHORT).show()
+            returnToMain()
         }
 
         builder.setNegativeButton("No"){dialog,which ->
@@ -46,7 +61,28 @@ class SettingsActivity : AppCompatActivity() {
         //do something here - new activity?
     }
 
-    private fun deleteAccount(){
-        //delete account from db and auth, return to MainActivity
+    fun deleteAuthAccount() {
+        //delete auth account
+        val user = mAuth.currentUser
+
+        user?.delete()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun deleteFromDatabase(){
+        //delete user from database
+        val userKey = mAuth.currentUser!!.uid
+        mDatabaseReference.child(userKey).removeValue()
+    }
+
+    private fun returnToMain(){
+        //go back to main activity, clear stack
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
     }
 }
