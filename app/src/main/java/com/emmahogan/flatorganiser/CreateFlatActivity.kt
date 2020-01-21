8b83a35c0b86a1d47d9721fb30fd2d7391bd8bbd
roonil-wazlib <elfhogan@gmail.com>
2020-01-21 15:41:35 +1300

    Firestore document creation broken. No idea why.

--- app/src/main/java/com/emmahogan/flatorganiser/CreateFlatActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/CreateFlatActivity.kt
@@ -2,11 +2,42 @@ package com.emmahogan.flatorganiser;
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import android.widget.Toast
+
 
 class CreateFlatActivity : AppCompatActivity() {
+
+    //get reference to Firebase Auth
+    private var mAuth = FirebaseAuth.getInstance()
+
+    //Initialise Firebase db
+    private var mDatabase = FirebaseDatabase.getInstance()
+    //Get reference to users child
+    private var mDatabaseReference = mDatabase.reference.child("users")
+
+    //get reference to Firestore Cloud instance
+    private var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_flat)
+
+        addFlatToDatabase()
+    }
+
+    private fun addFlatToDatabase(){
+        // Create a new flat
+        val flat = hashMapOf(
+            "name" to "Test",
+            "members" to listOf("User name")
+        )
+
+        db.collection("flats").document("LA")
+            .set(flat)
+            .addOnSuccessListener { Toast.makeText(this, "Flat created", Toast.LENGTH_SHORT).show() }
+            .addOnFailureListener { Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show() }
     }
 }
