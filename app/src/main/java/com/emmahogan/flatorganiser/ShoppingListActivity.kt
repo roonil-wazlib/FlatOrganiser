87305642297a9a8ab1c35809a0ac488a20ff9f41
roonil-wazlib <elfhogan@gmail.com>
2020-01-27 23:58:49 +1300

    Added add new item to shopping list functionality. Still no sync w db

--- app/src/main/java/com/emmahogan/flatorganiser/ShoppingListActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/ShoppingListActivity.kt
@@ -1,5 +1,8 @@
 package com.emmahogan.flatorganiser
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,18 +15,22 @@ class ShoppingListActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
     private var modelArrayList: ArrayList<ReModel>? = null
     private var customAdapter: ReAdapter? = null
-    private val groceriesList = arrayOf("Milk", "Eggs", "Cheese", "Fruit", "Flour", "Beans", "Carrots", "Bread")
+    private var groceriesList = mutableListOf("Milk", "Eggs", "Cheese", "Fruit", "Flour", "Beans", "Carrots", "Bread")
+
+    lateinit var newItemET : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_list)
 
         recyclerView = findViewById(R.id.recycler)
+        updateList()
 
-        modelArrayList = getModel(false)
-        customAdapter = ReAdapter(this, modelArrayList!!)
-        recyclerView!!.adapter = customAdapter
-        recyclerView!!.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+
+        //get reference to new item edit text and add button
+        newItemET = findViewById(R.id.new_grocery)
+        val addBtn : Button = findViewById(R.id.add)
+        addBtn.setOnClickListener{ addItem() }
     }
 
     private fun getModel(isSelect: Boolean): ArrayList<ReModel> {
@@ -36,5 +43,19 @@ class ShoppingListActivity : AppCompatActivity() {
             list.add(model)
         }
         return list
+    }
+
+    private fun addItem(){
+        val newItem = newItemET.getText().toString()
+        groceriesList.add(newItem)
+        Log.d("TAG", groceriesList.toString())
+        updateList()
+    }
+
+    private fun updateList(){
+        modelArrayList = getModel(false)
+        customAdapter = ReAdapter(this, modelArrayList!!)
+        recyclerView!!.adapter = customAdapter
+        recyclerView!!.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
     }
 }
