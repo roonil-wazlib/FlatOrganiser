b30c22e5da6e7c87e45cc202f8972214be448516
roonil-wazlib <elfhogan@gmail.com>
2020-01-29 23:59:03 +1300

    Added initial code for splash screen activity. Not yet functional, but would be better to use as loading screen to limit login issues.

---
+++ app/src/main/java/com/emmahogan/flatorganiser/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.emmahogan.flatorganiser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
