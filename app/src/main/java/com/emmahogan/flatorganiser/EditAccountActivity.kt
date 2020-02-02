ec8c8caaf450639ffcbae583c43989eac71cf2a8
roonil-wazlib <elfhogan@gmail.com>
2020-02-02 23:13:29 +1300

    Set up some basic layout structure for edit account

--- app/src/main/java/com/emmahogan/flatorganiser/EditAccountActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/EditAccountActivity.kt
@@ -1,6 +1,8 @@
 package com.emmahogan.flatorganiser
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class EditAccountActivity : AppCompatActivity() {
@@ -9,9 +11,19 @@ class EditAccountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_edit_account)
 
         //get user data
         currentUser = intent.getParcelableExtra("user")
+
+        //set up layout
+        val editNameET : EditText = findViewById(R.id.editName)
+        editNameET.setText(currentUser.name.toString())
+
+        val editEmailET : EditText = findViewById(R.id.editEmail)
+        editEmailET.setText(currentUser.email.toString())
+
+        val currentFlatTV : TextView = findViewById(R.id.currentFlat)
+        currentFlatTV.setText(currentUser.flat.toString())
     }
 }
