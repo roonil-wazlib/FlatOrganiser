a10f77f832256d8523d50d29e99e87bd196522b7
roonil-wazlib <elfhogan@gmail.com>
2020-02-05 00:37:07 +1300

    Leave flat method partially working

--- app/src/main/java/com/emmahogan/flatorganiser/EditAccountActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/EditAccountActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class EditAccountActivity : AppCompatActivity() {
@@ -30,12 +31,26 @@ class EditAccountActivity : AppCompatActivity() {
 
         val changeFlat : Button = findViewById(R.id.changeFlat)
         changeFlat.setOnClickListener{ changeFlat() }
+
+        val leaveFlat : Button = findViewById(R.id.leaveFlat)
+        leaveFlat.setOnClickListener{ leaveFlat() }
     }
 
 
     private fun changeFlat(){
         val intent = Intent(this, JoinFlatActivity::class.java)
         intent.putExtra("user", currentUser)
+        startActivity(intent)
+    }
+
+
+    private fun leaveFlat(){
+        //TODO run are you sure?
+        (CloudFirestore::deleteFromFlat)(CloudFirestore(), currentUser.flat.toString(), currentUser)
+        Toast.makeText(this, "Removing from flat...", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.putExtra("user", currentUser)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
     }
 }
