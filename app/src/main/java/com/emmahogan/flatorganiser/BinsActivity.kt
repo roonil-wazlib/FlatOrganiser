cc049cfe35fad6547857d31064f5eea53304ab2d
roonil-wazlib <elfhogan@gmail.com>
2020-02-05 23:56:24 +1300

    Added new empty activity for tracking which bins to put out.

---
+++ app/src/main/java/com/emmahogan/flatorganiser/BinsActivity.kt
@@ -0,0 +1,12 @@
+package com.emmahogan.flatorganiser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BinsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bins)
+    }
+}
