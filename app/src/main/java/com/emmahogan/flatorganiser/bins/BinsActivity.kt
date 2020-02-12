f12e7eec2d41caec1d05269c9c72ce3d3b1a54a3
roonil-wazlib <elfhogan@gmail.com>
2020-02-12 23:13:54 +1300

    Bins activity retrieves current date

--- app/src/main/java/com/emmahogan/flatorganiser/bins/BinsActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/bins/BinsActivity.kt
@@ -3,20 +3,27 @@ package com.emmahogan.flatorganiser.bins
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.emmahogan.flatorganiser.R
 import com.emmahogan.flatorganiser.auth.User
 import com.emmahogan.flatorganiser.display.HomeActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class BinsActivity : AppCompatActivity() {
 
     lateinit var currentUser : User
+    lateinit var date : String
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bins)
+        setContentView(com.emmahogan.flatorganiser.R.layout.activity_bins)
 
         //get user data
         currentUser = intent.getParcelableExtra("user")
+
+        date = getCurrentDate()
+
+        getCurrentDate()
     }
 
     override fun onBackPressed(){
@@ -24,5 +31,17 @@ class BinsActivity : AppCompatActivity() {
         intent.putExtra("user", currentUser)
         startActivity(intent)
         this.finish()
+    }
+
+
+    fun getCurrentDate() : String {
+        val c = Calendar.getInstance().getTime()
+        println("Current time => $c")
+
+        val df = SimpleDateFormat("dd/MMM/yyyy")
+        val formattedDate = df.format(c)
+
+        return formattedDate
+
     }
 }
