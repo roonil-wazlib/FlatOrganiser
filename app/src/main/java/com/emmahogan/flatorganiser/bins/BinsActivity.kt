73566c8b3f32d3942ef09a8ea2859ecd320add0d
roonil-wazlib <elfhogan@gmail.com>
2020-02-13 22:11:59 +1300

    Bin activity displays how often each bins goes out

--- app/src/main/java/com/emmahogan/flatorganiser/bins/BinsActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/bins/BinsActivity.kt
@@ -3,17 +3,15 @@ package com.emmahogan.flatorganiser.bins
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.emmahogan.flatorganiser.R
 import com.emmahogan.flatorganiser.auth.User
 import com.emmahogan.flatorganiser.display.HomeActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_bins.view.*
 import java.text.SimpleDateFormat
 import java.util.*
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.emmahogan.flatorganiser.bins.BinInfo
 
 
 
@@ -23,6 +21,7 @@ class BinsActivity : AppCompatActivity() {
     lateinit var currentUser : User
     lateinit var date : String
     var bins = HashMap<String, BinInfo>()
+    var binTV = HashMap<String, TextView>()
 
     private var db = FirebaseFirestore.getInstance()
 
@@ -34,6 +33,9 @@ class BinsActivity : AppCompatActivity() {
         currentUser = intent.getParcelableExtra("user")
 
         date = getCurrentDate()
+
+        //get textview objects
+        setUpTextViews()
 
 
         val docRef = db.collection("flats/${currentUser.flat.toString()}/data").document("bin_dates")
@@ -62,6 +64,18 @@ class BinsActivity : AppCompatActivity() {
     }
 
 
+    private fun setUpTextViews(){
+        val redTV : TextView = findViewById(R.id.red_bin)
+        binTV.put("red", redTV)
+        val yellowTV : TextView = findViewById(R.id.yellow_bin)
+        binTV.put("yellow", yellowTV)
+        val greenTV : TextView = findViewById(R.id.green_bin)
+        binTV.put("green", greenTV)
+        val blackTV : TextView = findViewById(R.id.black_bin)
+        binTV.put("black", blackTV)
+    }
+
+
     override fun onBackPressed(){
         val intent = Intent(this, HomeActivity::class.java)
         intent.putExtra("user", currentUser)
@@ -83,17 +97,25 @@ class BinsActivity : AppCompatActivity() {
 
     private fun populateDisplay(){
         for(bin in bins){
+            val colour = bin.key
             val day = getDayFromDate(bin.value.startDate)
-            Log.d("TAG", day)
+            val frequency = bin.value.frequency
+            updateTV(colour, day, frequency)
         }
     }
 
 
     private fun getDayFromDate(date : String) : String{
         val date = SimpleDateFormat("dd/M/yyyy").parse(date)
-        val day =  SimpleDateFormat("EE").format(date)
+        val day =  SimpleDateFormat("EEEE").format(date)
 
         return day.toString()
+    }
+
+
+    private fun updateTV(colour : String, day : String, frequency : String){
+        val format = "${colour.capitalize()} bin goes out every ${frequency} on ${day}s."
+        binTV[colour]!!.text = format
     }
 }
 
