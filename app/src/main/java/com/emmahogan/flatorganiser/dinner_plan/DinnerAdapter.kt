11aef67141713945e0d95239f19fc512e0b13346
roonil-wazlib <elfhogan@gmail.com>
2020-02-22 01:41:54 +1300

    Really shitty, half broken, barely readable, not remotely functional recycler view implemented for dinner plan activity. Days of the week don't even appear in order for Christ's sake. This is why you're not supposed to code at 2am in the god damn morning. Go tf to sleep.

---
+++ app/src/main/java/com/emmahogan/flatorganiser/dinner_plan/DinnerAdapter.kt
@@ -0,0 +1,80 @@
+package com.emmahogan.flatorganiser.dinner_plan
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.emmahogan.flatorganiser.CloudFirestore
+import com.emmahogan.flatorganiser.R
+import com.emmahogan.flatorganiser.auth.User
+import java.util.ArrayList
+
+class DinnerAdapter(private val context : Context, imageModelArrayListMain: ArrayList<DayItem>, currentUser: User) :
+    RecyclerView.Adapter<DinnerAdapter.MyViewHolder>() {
+
+
+    companion object {
+        lateinit var imageModelArrayList: ArrayList<DayItem>
+    }
+
+
+    private val inflater : LayoutInflater
+    var holderList = mutableListOf<MyViewHolder>()
+    var user = User()
+
+
+    init {
+        inflater = LayoutInflater.from(context)
+        imageModelArrayList = imageModelArrayListMain //list of ReModel class instances
+        user = currentUser
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : MyViewHolder {
+        //set layout for each item in recyclerview
+        val view = inflater.inflate(R.layout.single_day_dinner, parent, false)
+        val item =  MyViewHolder(view)
+        holderList.add(item)
+        return item
+    }
+
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        //set up each item view as it appears on screen while scrolling
+
+        holder.chefEditText.setText(imageModelArrayList[position].chef)
+        holder.day.setText(imageModelArrayList[position].day)
+    }
+
+
+    override fun getItemCount(): Int {
+        //get number of items in list
+        return imageModelArrayList.size
+    }
+
+
+    fun writeToDb(){
+        val listData = HashMap<String, Any>()
+        for (x in imageModelArrayList!!){
+            listData.put(x.day, x.chef)
+        }
+        (CloudFirestore::addShoppingList)(CloudFirestore(), user.flat.toString(), listData)
+    }
+
+
+    //the 'thing' that the data is stored in and gets recycled during scroll
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //inner class represents individual items within recylcerview
+        var mealEditText : EditText
+        var chefEditText : EditText
+        var day : TextView
+        init {
+            day = itemView.findViewById(R.id.day)
+            chefEditText = itemView.findViewById(R.id.chef)
+            mealEditText = itemView.findViewById(R.id.meal)
+        }
+    }
+}
--- app/src/main/java/com/emmahogan/flatorganiser/dinner_plan/DinnerPlanActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/dinner_plan/DinnerPlanActivity.kt
@@ -1,14 +1,24 @@
 package com.emmahogan.flatorganiser.dinner_plan
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.emmahogan.flatorganiser.CloudFirestore
 import com.emmahogan.flatorganiser.R
 import com.emmahogan.flatorganiser.auth.User
+import kotlinx.android.synthetic.main.single_day_dinner.*
+import java.util.ArrayList
 
 class DinnerPlanActivity : AppCompatActivity() {
 
-
+    private var recyclerView: RecyclerView? = null
+    private var modelArrayList: ArrayList<DayItem>? = null
+    private var customAdapter: DinnerAdapter? = null
     lateinit var currentUser : User
+
+    private lateinit var mealsList : HashMap<String, Any>
 
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
@@ -16,5 +26,49 @@ class DinnerPlanActivity : AppCompatActivity() {
 
         //get user data
         currentUser = intent.getParcelableExtra("user")
+
+        recyclerView = findViewById(R.id.recycler)
+
+        //get reference to new item edit text and add button
+        val saveBtn : Button = findViewById(R.id.save)
+
+        mealsList = HashMap<String, Any>()
+
+        val days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+        for (day in days){
+            mealsList.put(day, "")
+        }
+        createList()
+    }
+
+    private fun createModel(): ArrayList<DayItem> {
+        val list = ArrayList<DayItem>()
+        for ((key, value) in mealsList) {
+
+            //get class instance for each item in list
+            val model = DayItem()
+            model.day = key
+            model.meal = value.toString()
+            model.chef = value.toString()
+            list.add(model)
+        }
+        return list
+    }
+
+
+    private fun createList(){
+        modelArrayList = createModel()
+        customAdapter = DinnerAdapter(this, modelArrayList!!, currentUser)
+        recyclerView!!.adapter = customAdapter
+        recyclerView!!.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+    }
+
+
+    fun writeToDb(){
+        val listData = HashMap<String, Any>()
+        for (x in modelArrayList!!){
+            listData.put(x.day, x.chef)
+        }
+        (CloudFirestore::addShoppingList)(CloudFirestore(), currentUser.flat.toString(), listData)
     }
 }
