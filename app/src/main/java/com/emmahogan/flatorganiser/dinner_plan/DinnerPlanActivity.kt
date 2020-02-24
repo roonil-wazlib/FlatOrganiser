c8811b2a37f11bfa251bc915b853abe5729b9c37
roonil-wazlib <elfhogan@gmail.com>
2020-02-24 23:36:15 +1300

    Added read from db function to DinnerPlanActivity. Doesn't seperate different data into correct textview yet but it works

--- app/src/main/java/com/emmahogan/flatorganiser/dinner_plan/DinnerPlanActivity.kt
+++ app/src/main/java/com/emmahogan/flatorganiser/dinner_plan/DinnerPlanActivity.kt
@@ -1,6 +1,7 @@
 package com.emmahogan.flatorganiser.dinner_plan
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.emmahogan.flatorganiser.CloudFirestore
 import com.emmahogan.flatorganiser.R
 import com.emmahogan.flatorganiser.auth.User
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_dinner_plan.*
 import kotlinx.android.synthetic.main.single_day_dinner.*
 import java.util.ArrayList
@@ -22,6 +24,8 @@ class DinnerPlanActivity : AppCompatActivity() {
     private lateinit var mealsList : HashMap<String, Any>
     private var days  = listOf<String>()
 
+    private var db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dinner_plan)
@@ -30,17 +34,35 @@ class DinnerPlanActivity : AppCompatActivity() {
         currentUser = intent.getParcelableExtra("user")
         recyclerView = findViewById(R.id.recycler)
 
-
+        days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+        
         val saveBtn : Button = findViewById(R.id.save)
         saveBtn.setOnClickListener{save()}
 
         mealsList = HashMap()
 
-        days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
-        for (day in days){
-            mealsList.put(day, "")
-        }
-        createList()
+        val docRef = db.collection("flats/${currentUser.flat.toString()}/data").document("dinner_plans")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("TAG", "DocumentSnapshot data: ${document.data}")
+                    try{mealsList = document.data as HashMap<String, Any>}
+                    catch(e: TypeCastException){
+                        for (day in days){
+                            mealsList.put(day, "")
+                        }
+                    }
+                    createList()
+                } else {
+                    Log.d("TAG", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("TAG", "get failed with ", exception)
+            }
+
+
+
     }
 
     private fun createModel(): ArrayList<DayItem> {
